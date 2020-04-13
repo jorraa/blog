46e6714fb6db963f04cdc9035abd9ea247966d7a
Jorma Raatikainen <raatikainen.jorma@gmail.com>
2020-04-13 11:15:40 +0300

    Tarvii siistimistä paljon

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -16,23 +16,75 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog  = (blogs) => {
+  if(blogs.length === 0) { return {} }
   const likesArray = blogs.map(blog => blog.likes)
   return blogs[likesArray
     .findIndex((likes) => likes === Math.max(...likesArray))]
 }
 
 const mostBlogs = (blogs) => {
+  if(blogs.length === 0) {
+    return { 'author': '', 'blogsCount': 0 }
+  }
+  // countsObj [{author, countOfBlogs} ...]
   const countsObj = lodash.countBy(blogs.map(blog => blog.author))
-  console.log('Object.values(countsObj)', Object.values(countsObj))
-  const vals = Object.values(countsObj)
-  const maxVal = Math.max(...vals)
-  console.log('max', maxVal)
-  const authors = Object.entries(countsObj).find( entry => {
-    console.log('entry', entry)
-    return entry[1] === maxVal
+  const maxCount = Math.max(...Object.values(countsObj))
+
+  const author = Object.entries(countsObj)
+    .find( entry => {
+      return entry[1] === maxCount
+    })[0] // at least one author
+  return { 'author': author, 'blogsCount': maxCount }
+}
+
+const mostLikes = (blogs) => {
+  if(blogs.length === 0) {
+    return { 'author': '', 'likesCount': 0 }
+  }
+  const pickedArr = blogs.map(blog => [blog.author, blog.likes])
+  console.log('picked', pickedArr)
+  const likesObj = lodash.groupBy(pickedArr, function(arr) {
+    return arr[0]
   })
-  return authors[0]
+  console.log('likesObj', likesObj)
+
+  let resArr = []
+  Object.entries(likesObj).forEach( o => {
+    console.log('OOO', o)
+    let sum = 0
+    Object.values(o[1]).forEach(v => {
+      sum += v[1]
+    })
+    console.log('sum',sum)
+    o[1] = sum
+    console.log('OOOsum', o)
+    resArr.push(o)
+  })
+  console.log('resArr----', resArr)
+  const likesArray = resArr.map(author => author[1])
+  const maxCount = Math.max(...likesArray)
+  console.log('max', maxCount)
+  console.log('found', resArr.find(o => o[1] === maxCount))
+
+
+  //console.log('reduced', reduced2)
+
+
+  /*
+  // countsObj [{author, countOfBlogs} ...]
+  const countsObj = lodash.countBy(blogs.map(blog => blog.author))
+  const maxVal = Math.max(...Object.values(countsObj))
+
+  const author = Object.entries(countsObj)
+    .find( entry => {
+      return entry[1] === maxVal
+    })[0] // at least one author
+  return { 'author': author, 'blogsCount': maxVal }
+  */
+  const author = ''
+  const likesCount = 0
+  return { 'author': author, 'likesCount': likesCount }
 }
 module.exports = {
-  dummy, totalLikes, favoriteBlog, mostBlogs
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
