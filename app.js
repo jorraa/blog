ac4988555b519619fd29cbdd325bb966527d402c
Jorma Raatikainen <raatikainen.jorma@gmail.com>
2020-04-14 10:10:32 +0300

    Kurssin kohdan 4.3-4.4 tehty lukiessa, no testing

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('useFindAndModify', false)
 /* FOR TESTING PURPOSES tee malli tälle
@@ -40,8 +42,9 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
-
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use('/api/login', loginRouter)
 
 module.exports = app
