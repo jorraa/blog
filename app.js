5e96a1a87851a511b77435a723f2cc7be149c5f7
Jorma Raatikainen <raatikainen.jorma@gmail.com>
2020-04-15 17:14:44 +0300

    Siivousta

--- app.js
+++ app.js
@@ -10,24 +10,8 @@ const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const muuRouter = require('./controllers/testaa')
-
 mongoose.set('useFindAndModify', false)
-/* FOR TESTING PURPOSES tee malli tälle
-const list_helper = require('./utils/list_helper')
-console.log('FOR TESTING PURPOSES')
-const listSixBlogs = [
-  { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0 },
-  { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0 },
-  { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0 },
-  { _id: '5a422b891b54a676234d17fa', title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10, __v: 0 },
-  { _id: '5a422ba71b54a676234d17fb', title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0, __v: 0 },
-  { _id: '5a422bc61b54a676234d17fc', title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 2, __v: 0 }
-]
 
-list_helper.mostLikes(listSixBlogs)
-console.log('------- FOR TESTING PURPOSES END')
-// FOR TESTING PURPOSES END */
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -48,8 +32,6 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-
-app.use('/api/muu', muuRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
